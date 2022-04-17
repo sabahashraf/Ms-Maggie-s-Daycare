99f7808288a5d9cf4f279d6171d65e8e94658233
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-17 05:34:10 -0700

    checkout page designed

--- src/pages/Checkout/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,40 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Checkout = () => {
   const { serviceId } = useParams();
+  const handleCheckOut = (event) => {
+    event.preventDefault();
+    toast("Thank you for the booking");
+  };
   return (
     <div>
-      <h2>This is checkout</h2>
+      <h2 className="text-center text-purple my-5">Confirm Your Booking</h2>
+      <form onSubmit={handleCheckOut}>
+        <input type="text" name="name" id="" placeholder="Your name" required />
+        <input
+          type="email"
+          name="email"
+          id=""
+          placeholder="Your email"
+          required
+        />
+
+        <input
+          type="text"
+          name="address"
+          id=""
+          placeholder="Your address"
+          required
+        />
+
+        <input type="number" name="phone" id="" placeholder="Phone#" required />
+
+        <input id="signUpBtn" type="submit" value="Checkout "></input>
+      </form>
+      <ToastContainer />
     </div>
   );
 };
