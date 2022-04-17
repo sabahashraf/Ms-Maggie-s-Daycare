e1d1efbb56346eb7eee6ed4ea780b765a35bc306
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-16 18:13:07 -0700

    blog page done without styleing

--- src/pages/Blogs/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs/Blogs.js
@@ -3,9 +3,56 @@ import "./Blogs.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h2>I am blog</h2>
-    </div>
+    <section>
+      <h3>Difference betweeen authorization and authentication?</h3>
+      <p>
+        In simple words, authentication means who a person is.it can be verified
+        by his name, email address, password, date of birth, address and many
+        more. On the otherhand,authorization means the resources an
+        authenticated user can access.Access to resouces can be different from
+        user to user or person to person.Like in a website a buyer /user can
+        place an order but he is not authorized to change product's value.Again
+        admin can change product's value, he is authorized to do that.Thus
+        Authorization always takes place after authentication.
+      </p>
+      <h3>
+        {" "}
+        Why are you using firebase? What other options do you have to implement
+        authentication?
+      </h3>
+      <p>
+        Firebase is very easy to set up .Most of the cases you need front end
+        knowledges to use it.It gives you built in functions to create
+        authentication process for your project.Thus developers save
+        developmentment time and increase productivity.Expect authentication it
+        has services like cloud functions,realtime database,cloud
+        firestore,cloud messaging and hosting.
+      </p>
+      <p>
+        besides firebase,you can do token authentication,standard
+        authentication.multifactor authentication,passwordless authentication
+        ,social authentication to implement authentication for your project.
+      </p>
+      <h3>
+        What other services does firebase provide other than authentication
+      </h3>
+      <p>
+        Firebase is a full package that can help for developing apps and
+        websites faster .Some of the services are:
+        <ul>
+          <li>Cloud Firestore</li>
+          <li>Cloud Functions</li>
+          <li>Authentication</li>
+          <li>Hosting</li>
+          <li>Cloud Storage</li>
+          <li>Google Analytics</li>
+          <li>Predictions</li>
+          <li>Cloud Messaging</li>
+          <li>Dynamic Links</li>
+          <li>Remote Config</li>
+        </ul>
+      </p>
+    </section>
   );
 };
 
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../../../images/daycareLogo.png";
 
 const Navbar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} height={30} alt="daycare" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
