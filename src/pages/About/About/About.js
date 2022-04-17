87e10e383bbb12a82d7a55d1de24ec278470ddb7
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-16 18:34:45 -0700

    working on about page

--- src/pages/About/About/About.js
+++ src/pages/About/About/About.js
@@ -3,9 +3,25 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h2>This is about</h2>
-    </div>
+    <section>
+      <div>
+        <h2>It's about me...</h2>
+        <p>
+          I'm Sabah.I had done bachelor of science in elctrical and electronics
+          engineering.While I was completing my degree I was introduced with C
+          programming language.That's when I felt that I like programming and
+          computer science rather than electrical engineering.That's why after
+          completing my degree I was looking for courses where I can find my
+          passion and thus one day I found web development.First I started
+          learning it by buying an udemy course.But while I decided to take web
+          development as a career I enrolled in Programming hero's web
+          development course.I am currently doing this course.I am working
+          really hard ,trying to give my 100 percent,giving my learning priority
+          over everything else so that I can successfully finish the course and
+          get ready to enter in my professional life.
+        </p>
+      </div>
+    </section>
   );
 };
 
